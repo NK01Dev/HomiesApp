a8a787e6b2a5d2f27d5f0a4e0ebee45f608f7b17
NK01Dev <kamal.nk.naim@gmail.com>
2025-08-27 12:49:25 +0100

    c

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  static const String appName = String.fromEnvironment('APP_NAME');
+  static const String supabaseUrl = String.fromEnvironment('SUPABASE_URL');
+  static const String supabaseAnonKey = String.fromEnvironment('SUPABASE_ANON_KEY');
+
+  static const double defaultPadding = 16.0;
+  static const double defaultRadius = 12.0;
+}
